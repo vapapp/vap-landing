2d658491e98e52752fb0d9256b05a9bb001ef80d
Kauê Cavalcante <Kauecavalcante4@gmail.com>
2025-08-26 15:04:59 -0300

    chore: Primeiro deploy no novo servidor

--- src/app/components/ui/TerminalLoader.tsx
+++ src/app/components/ui/TerminalLoader.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import styles from './TerminalLoader.module.css';
 
-
+// testando servidor
 interface TerminalLoaderProps {
   className?: string;
 }
